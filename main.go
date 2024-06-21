3b0d33e5b0e84941cd6bd4dacd3fe2e527eda657
Ang Chin Han <ang.chin.han@gmail.com>
2024-06-21 11:32:58 +0800

    fix: More correct output

--- main.go
+++ main.go
@@ -2,36 +2,62 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
-	"sort"
 	"strings"
 )
 
+func rmHidden(s []fs.DirEntry) []fs.DirEntry {
+	src, dst := 0, 0
+	for src < len(s) {
+		if s[src].Name()[0] != '.' {
+			s[dst] = s[src]
+			dst++
+		}
+		src++
+	}
+	s = s[:dst]
+	return s
+}
+
 func dir(prev string, f string, n string) {
-	files, err := os.ReadDir(f)
+	files, err := os.ReadDir(f) // os.ReadDir() returns *sorted* entries
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Not much diff, esp since os.ReadDir() returns sorted entries
+	// files = rmHidden(files)
 
-	sort.Slice(files, func(i, j int) bool {
-		return files[i].Name() < files[j].Name()
-	})
 	if n == "" {
 		fmt.Println(f)
-	} else {
-		fmt.Println(prev[:len(prev)-1], "├──", n)
 	}
 	l := prev + "├──"
-	for _, file := range files {
+	for k, file := range files {
 		n := file.Name()
 		if n[0] == '.' {
 			continue
 		}
-		if file.IsDir() {
-			dir(prev+"│  ", f+"/"+n, n)
-		} else {
+		isLast := false
+		if k == len(files)-1 {
+			isLast = true
+		}
+
+		if !isLast {
 			fmt.Println(l, n)
+		} else {
+			if len(prev) > 0 {
+				fmt.Println(prev[:len(prev)-1], "└──", n)
+			} else {
+				fmt.Println("└──", n)
+			}
+		}
+		if file.IsDir() {
+			if !isLast {
+				dir(prev+"│   ", f+"/"+n, n)
+			} else {
+				dir(prev+"    ", f+"/"+n, n)
+			}
 		}
 	}
 }
